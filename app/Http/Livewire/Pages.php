3f574d22721cb3d9e5a8284baa18abaff45ea929
Christopher Carlos <cscarlos@iskolarngbayan.pup.edu.ph>
2021-10-30 15:22:18 +0800

    design and summernote in pages

--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -5,18 +5,17 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use App\Models\Page;
 
-use App\Http\Livewire\Objects;
-
 use Illuminate\Validation\Rule;
 use Livewire\withPagination;
 
 use Illuminate\Support\STR;
-use Illuminate\Support\Facades\DB;
+
 
 class Pages extends Component
 {
     use WithPagination;
     public $modalFormVisible = false;
+    public $updatemodalFormVisible = false;
     public $modelConfirmDeleteVisible = false;
     public $modelId;
     public $slug;
@@ -25,15 +24,65 @@ class Pages extends Component
     public $isSetToDefaultHomePage;
     public $isSetToDefaultNotFoundPage;
 
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'slug' => ['required', Rule::unique('pages','slug')->ignore($this->modelId)],
+            'content' => 'required',
+        ];
+    }
 
+    public function mount()
+    {
+        $this->resetPage();
+    }
 
+    public function updatedtitle($value)
+    {
+        // $this->slug = $value;
+        $this->slug = Str::slug($value);
+    }
+
+    // public function generateSlug($value)
+    // {
+    //     $process1 = str_replace(' ', '-',$value);
+    //     $process2 = strtolower($process1);
+    //     $this->slug = $process2;
+    // }
 
     public function create()
     {
         $this->resetValidation();
+        // dd($this);
         $this->validate(); 
         $this->unassignedDefaultHomePage(); 
-        $this->unassignedDefaultNotFoundPage(); 
+        $this->unassignedDefaultNotFoundPage();
+
+        $this->validate([
+            'title' => 'required',
+            'slug' => 'required',
+            'content' => 'required',
+        ]);
+ 
+       $content = $this->content;
+       $dom = new \DomDocument();
+       $dom->loadHtml($content, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+       $imageFile = $dom->getElementsByTagName('imageFile');
+       foreach($imageFile as $item => $image){
+           $data = $img->getAttribute('src');
+
+           list($type, $data) = explode(';', $data);
+           list(, $data)      = explode(',', $data);
+
+           $imgeData = base64_decode($data);
+           $image_name= "/upload/" . time().$item.'.png';
+           $path = public_path() . $image_name;
+           file_put_contents($path, $imgeData);
+           $image->removeAttribute('src');
+           $image->setAttribute('src', $image_name);
+        }
+        $content = $dom->saveHTML();
         Page::create($this->modelData());
         $this->modalFormVisible = false;
         $this->reset(); 
@@ -70,28 +119,18 @@ class Pages extends Component
         $this->unassignedDefaultHomePage(); 
         $this->unassignedDefaultNotFoundPage(); 
         Page::find($this->modelId)->update($this->modelData());
-        $this->modalFormVisible = false;
+        $this->updatemodalFormVisible = false;
         $this->reset();
-    }
-
-    public function updateModal()
-    {
-        return [
-            'title' => $this->title,
-            'slug' => $this->slug,
-            'content' => $this->content,
-        ];
     }
 
     public function updateShowModal($id)
     {
+        // dd($this->content);
         $this->resetValidation();
         $this->reset();
         $this->modelId = $id;
-        // dd($this->modelId);
-        $this->modalFormVisible = true;
-        
         $this->loadModel();
+        $this->updatemodalFormVisible = true;
     }
 
     public function updatedisSetToDefaultHomePage()
@@ -152,24 +191,7 @@ class Pages extends Component
 
 
 
-    public function rules()
-    {
-        return [
-            'title' => 'required',
-            'slug' => 'required',
-        ];
-    }
-
-    public function mount()
-    {
-        $this->resetPage();
-    }
-
-    public function updatedtitle($value)
-    {
-        $this->slug = Str::slug($value);
-    }
-
+    // liveware data rendering
     public function render()
     {
         return view('livewire.pages',[
--- resources/views/livewire/pages.blade.php
+++ resources/views/livewire/pages.blade.php
@@ -1,4 +1,17 @@
 <div class="p-6">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
+    <style>
+        .modal-backdrop {
+          z-index: -1;
+        }
+    </style>
     <div class="flex items-center justify-end px-4 py-3 text-right sm:px-6">
         <x-jet-button wire:click="createShowModel">
             {{ __('Create') }}
@@ -15,7 +28,7 @@
                             <tr>
                                 <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Title</th>
                                 <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Link</th>
-                                <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Content</th>
+                                <!-- <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Content</th> -->
                                 <th class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Action</th>
                             </tr>
                         </thead>
@@ -33,9 +46,10 @@
                                                 {{ $item->slug }}
                                             </a>
                                         </td>
-                                        <td class="px-6 py-4 text-sm whitespace-no-wrap">
+                                       <!--  <td class="px-6 py-4 text-sm whitespace-no-wrap">
                                             {!! \Illuminate\Support\Str::limit($item->content, 50, '...') !!}
-                                        </td>
+                                            {!! $item->content !!}
+                                        </td> -->
                                         <td class="px-6 py-4 text-sm text-right">
                                             <x-jet-button wire:click="updateShowModal({{ $item->pages_id }})">
                                                 {{__('Update')}}
@@ -66,7 +80,6 @@
 
 
 <!-- MODALS -->
-<!-- CREATE MODALS -->
     <x-jet-dialog-modal wire:model="modalFormVisible">
             <x-slot name="title">
                 {{ __('Save Page') }} {{$modelId}}
@@ -114,12 +127,7 @@
                     <div class="rounded-md shadow-sm">
                         <div class="mt-1 bg-white">
                             <div class="body-content" wire:ignore>
-                                <trix-editor
-                                    class="trix-content"
-                                    x-ref="trix"
-                                    wire:model.debounce.100000ms="content"
-                                    wire:key="trix-content-unique-key"
-                                ></trix-editor>
+                                <textarea type="text" input="content" id="summernote" class="form-control summernote"></textarea>
                             </div>
                         </div>
                     </div>
@@ -135,17 +143,85 @@
                 <x-jet-secondary-button wire:click="$toggle('modalFormVisible')" wire:loading.attr="disabled">
                     {{ __('Cancel') }}
                 </x-jet-secondary-button>
-
-                @if($modelId)
-                    <x-jet-secondary-button class="ml-2" wire:click="update" wire:loading.attr="disabled">
-                        {{ __('Update Page') }}
-                    </x-jet-secondary-button>                    
-                @else
                     <x-jet-secondary-button class="ml-2" wire:click="create" wire:loading.attr="disabled">
                         {{ __('Create Page') }}
                     </x-jet-secondary-button>
-                @endif
 
+            </x-slot>
+        </x-jet-dialog-modal>
+
+<!--UPDATE MODALS -->
+    <x-jet-dialog-modal wire:model="updatemodalFormVisible">
+            <x-slot name="title">
+                {{ __('Update Page') }} {{$modelId}}
+            </x-slot>
+
+            <x-slot name="content">
+                <div class="mt-4">
+                    <x-jet-label for="title" value="{{ __('Title') }}" />
+                    <x-jet-input id="title" style="color:red" class="block mt-1 w-full" type="text" wire:model.debounce.800ms="title" required autofocus />
+
+                    @error('title')
+                        <span class="error">{{ $message }}</span>
+                    @enderror
+
+                </div>
+                <div class="mt-4">
+                    <x-jet-label for="slug" value="{{ __('Slug') }}" />
+                    <div class="mt-1 flex rounded-md shadow-sm">
+                        <span class="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
+                            http://localhost:8000/
+                        </span>
+                        <input wire:model="slug" class="form-input flex-1 block w-full rounded-none rounded-r-md transition duration-150 ease-in-out sm:text-sm sm:leading-5" placeholder="url-slug">
+                    </div>
+                </div>
+
+                    @error('slug')
+                        <span class="error">{{ $message }}</span>
+                    @enderror
+
+                 <div class="mt-4">
+                    <label>
+                        <input class="form-checkbox" type="checkbox" value="{{ $isSetToDefaultHomePage }}" wire:model="isSetToDefaultHomePage"/>
+                        <span class="ml-2 text-sm text-gray-600">Set as the default home page</span>
+                    </label>
+                </div>
+                <div class="mt-4">
+                    <label>
+                        <input class="form-checkbox" type="checkbox" value="{{ $isSetToDefaultNotFoundPage }}" wire:model="isSetToDefaultNotFoundPage"/>
+                        <span class="ml-2 text-sm text-red-600">Set as the default 404 error page</span>
+                    </label>
+                </div>
+
+                <div class="mt-4">
+                    <x-jet-label for="content" value="{{ __('Content') }}" />
+                    <div class="rounded-md shadow-sm">
+                        <div class="mt-1 bg-white">
+                            <div class="body-content" wire:ignore>
+                                <textarea type="text" input="content" id="summernote" class="form-control summernote">
+                                    <?php echo $content; ?>
+                                </textarea>
+                                <!-- <input id="summernote" class="form-control summernote"/> -->
+                                <!-- <textarea type="text" input="content" id="summernote" class="form-control summernote">{!! $content !!}</textarea> -->
+                                <!-- <input type="text" name="content" id="summernote"  class="form-control"></input> -->
+                            </div>
+                        </div>
+                    </div>
+
+                    @error('content')
+                        <span class="error">{{ $message }}</span>
+                    @enderror
+
+                </div>
+            </x-slot>
+
+            <x-slot name="footer">
+                <x-jet-secondary-button wire:click="$toggle('updatemodalFormVisible')" wire:loading.attr="disabled">
+                    {{ __('Cancel') }}
+                </x-jet-secondary-button>
+                    <x-jet-secondary-button class="ml-2" wire:click="update" wire:loading.attr="disabled">
+                        {{ __('Update Page') }}
+                    </x-jet-secondary-button>                    
             </x-slot>
         </x-jet-dialog-modal>
 
@@ -169,5 +245,48 @@
                 </x-jet-danger-button>
             </x-slot>
         </x-jet-dialog-modal>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script>
+    $(document).ready(function() {
+        $('.summernote').summernote(
+        // 'pasteHTML',$content
+        {
+        focus: true,
+        tabsize: 2,
+        height: 200,
+        toolbar: [
+          ['style', ['style']],
+          ['font', ['bold', 'underline', 'clear']],
+          ['color', ['color']],
+          ['para', ['ul', 'ol', 'paragraph']],
+          ['table', ['table']],
+          ['insert', ['link', 'picture', 'video']],
+          ['view', ['fullscreen', 'codeview', 'help']]
+        ],
+        callbacks: {
+          onChange: function(contents, $editable) {
+          @this.set('content', contents);
+        }
+        }
+        });
+    });
+</script>
+
+
+
+
 
 </div>
