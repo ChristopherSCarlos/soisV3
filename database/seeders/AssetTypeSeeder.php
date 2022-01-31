d40ee0ddef126b29b5c29db653a7bbfb46cdb780
ChristianJAYs <jcabiles1488@gmail.com>
2022-02-01 03:13:50 +0800

    seeder
    pre pecheck naman ako kung tio lang seeder natin?

--- database/seeders/AssetTypeSeeder.php
+++ database/seeders/AssetTypeSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 class AssetTypeSeeder extends Seeder
 {
     /**
@@ -13,6 +14,37 @@ class AssetTypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $currentTime = Carbon::now();
+        $data = [
+            [
+                'asset_type_id' => 1,
+                'type' => 'Logo',
+                'asset_type_description' => 'This asset type is used for the logo of the page',
+                'created_at' => $currentTime,
+                'updated_at' => $currentTime,
+            ],
+            [
+                'asset_type_id' => 2,
+                'type' => 'Banner',
+                'asset_type_description' => 'This asset type is used for the logo of the page',
+                'created_at' => $currentTime,
+                'updated_at' => $currentTime,
+            ],
+            [
+                'asset_type_id' => 1,
+                'type' => 'Carousel',
+                'asset_type_description' => 'This asset type is used for the logo of the page',
+                'created_at' => $currentTime,
+                'updated_at' => $currentTime,
+            ],
+            [
+                'asset_type_id' => 1,
+                'type' => 'Featured News Image',
+                'asset_type_description' => 'This asset type is used for the logo of the page',
+                'created_at' => $currentTime,
+                'updated_at' => $currentTime,
+            ],
+        ];
+        DB::table('asset_types')->insert($data);
     }
 }
--- database/seeders/PageTypeSeeder.php
+++ database/seeders/PageTypeSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 class PageTypeSeeder extends Seeder
 {
     /**
@@ -13,6 +14,44 @@ class PageTypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $currentTime = Carbon::now();
+        $data = [
+            [
+                'page_types_id' => 1,
+                'page_type' => 'system page',
+                'page_description' => 'This page type is used for the system pages the system is using',
+                'created_at' => $currentTime,
+                'updated_at' => $currentTime,
+            ],
+            [
+                'page_types_id' => 2,
+                'page_type' => 'news',
+                'page_description' => 'This page type is used for the news page',
+                'created_at' => $currentTime,
+                'updated_at' => $currentTime,
+            ],
+            [
+                'page_types_id' => 3,
+                'page_type' => 'announcement',
+                'page_description' => 'This page type is used for the announcement page',
+                'created_at' => $currentTime,
+                'updated_at' => $currentTime,
+            ],
+            [
+                'page_types_id' => 4,
+                'page_type' => 'organizations',
+                'page_description' => 'This page type is used for the organizations page',
+                'created_at' => $currentTime,
+                'updated_at' => $currentTime,
+            ],
+            [
+                'page_types_id' => 5,
+                'page_type' => 'default interfaces',
+                'page_description' => 'This page type is used for the default interfaces page',
+                'created_at' => $currentTime,
+                'updated_at' => $currentTime,
+            ],
+        ];
+        DB::table('asset_types')->insert($data);
     }
 }
