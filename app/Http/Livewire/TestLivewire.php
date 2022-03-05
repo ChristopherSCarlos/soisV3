d3eb575f04d3854a597fac168ca7b41f5877246e
ChristianJAYs <jcabiles1488@gmail.com>
2022-03-06 03:07:19 +0800

    Update TestLivewire.php

--- app/Http/Livewire/TestLivewire.php
+++ app/Http/Livewire/TestLivewire.php
@@ -6,8 +6,16 @@ use Livewire\Component;
 
 class TestLivewire extends Component
 {
+
+    public function test()
+    {
+        dd("contrller");
+    }
+
     public function render()
     {
-        return view('livewire.test-livewire');
+        return view('livewire.test-livewire',[
+            'testcontorl' => $this->test(),
+        ]);
     }
 }
