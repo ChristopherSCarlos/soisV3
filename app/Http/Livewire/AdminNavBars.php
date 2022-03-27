e8810c96df6bbcedda2e18c4bbc02022e3f8a6f4
ChristianJAYs <jcabiles1488@gmail.com>
2022-03-27 19:24:30 +0800

    Update AdminNavBars.php

--- app/Http/Livewire/AdminNavBars.php
+++ app/Http/Livewire/AdminNavBars.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use Livewire\Component;
 use App\Models\User;
 use App\Http\Livewire\Objects;
+use App\Http\Controllers\PermissionCheckerController;
 
 use Auth;
 use Illuminate\Support\Facades\DB;
@@ -15,19 +16,46 @@ class AdminNavBars extends Component
     public $userId;
     public $userData;
     public $userRole;
+    public $userPermission;
+    private $permssionChecker;
 
     private $roleData;
     private $role_user_data;
     private $object;
+    private $user_data;
+
+    public function mount()
+    {
+       // $this->user_data = User::find(Auth::id()); 
+       //  $this->userRole = $this->user_data->roles->first();
+       //  dd($this->userRole);
+    }
+
+    public function getUserPermission()
+    {
+        $this->user_data = User::find(Auth::id()); 
+        $this->userPermission = $this->user_data->permissions()->get();
+        return $this->userPermission; 
+    }
 
     public function getUserData()
     {
+        // $userData = User::findOrFail(Auth::id());
+        // dd($userData->permissions);
+        // foreach ($userData->roles as $role) {
+        //     echo $role->pivot->role;
+        // }
+        // echo "<br><br>";
+        // dd(DB::table('permission_user')->where('user_id','=',Auth::id())->get());
+        // $this->permssionChecker = new PermissionCheckerController();
+        // $this->permssionChecker->test('helllo');
+        // dd(DB::table());
         // $this->object = new Objects();
         // $this->userRole = $this->object->roles();
         $this->role_user_data = DB::table('role_user')->where('user_id','=',Auth::id())->first();
         // dd($this->role_user_data->role_id);
         $this->roleData = DB::table('roles')->where('role_id','=',$this->role_user_data->role_id)->first();
-        // dd($this->userRole->role);
+        // dd($this->roleData->role);
         return $this->roleData->role; 
     }
     public function logoutControll()
@@ -41,6 +69,7 @@ class AdminNavBars extends Component
     {
         return view('livewire.admin-nav-bars',[
             'getUserRole' => $this->getUserData(),
+            'getUserPerms' => $this->getUserPermission(),
         ]);
     }
 }
