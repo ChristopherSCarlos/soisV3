7d7e49733b8a3dd34882939ad9cefbe325538d06
ChristianJAYs <61613108+ChristianJAYs@users.noreply.github.com>
2022-06-23 17:02:34 +0800

    Update my-application-academic.blade.php

--- resources/views/livewire/my-application-academic.blade.php
+++ resources/views/livewire/my-application-academic.blade.php
@@ -47,7 +47,7 @@
         </div>
         <div class="mx-auto">
             <h2 class="table-title">My Academic Applications</h2>
-            <div class="flex flex-col items-center">
+            <div class="flex flex-col lg:items-center">
                 <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                     <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
